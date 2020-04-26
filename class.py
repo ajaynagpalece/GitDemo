95741ecd9b72bd7f147d5624e69fa64f66989981
ajaynagpalece <ajaynagpal.ece@gmail.com>
2020-04-26 15:54:29 +0530

    Class file added by Ajay

---
+++ class.py
@@ -0,0 +1,27 @@
+class Calculator:
+    num = 100
+
+    def getData(self):
+        print("Method executed in class")
+
+
+obj = Calculator()              # Syntax to create object in python.
+obj.getData()
+print(obj.num)
+
+print("----------------------------Constructor----------------------------------")
+
+
+class Calculator:
+    num = 100
+
+    def __init__(self):
+        print("Constructor called automatically when object is created")
+
+    def getData(self):
+        print("Method executed in class")
+
+
+obj = Calculator()              # Syntax to create object in python.
+obj.getData()
+print(obj.num)
